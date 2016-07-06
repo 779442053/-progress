4898929790929aa9db470726a03a3ba5dc0e40c2
张威威 <779442053@qq.com>
2016-07-06 11:21:50 +0800

    Initial Commit

---
+++ 张威封装progress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  张威封装progress
+//
+//  Created by SH01 on 16/7/6.
+//  Copyright © 2016年 SH01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 张威封装progress/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  张威封装progress
+//
+//  Created by SH01 on 16/7/6.
+//  Copyright © 2016年 SH01. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
